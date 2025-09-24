7da38c83fbbee30bc55c28549fc398044dfe3f2f
Akash Mohan <akhikm4@gmail.com>
2025-09-23 21:18:17 -0500

    Working backend request logic + env safety

--- frontend/src/components/FileUploader.tsx
+++ frontend/src/components/FileUploader.tsx
@@ -1,10 +1,14 @@
 // CSV file uploader component for landing page will go here
+"use client";
+
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function FileUploader() {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [loading, setLoading] = useState(false);
+  const BACKEND_ROUTE =
+    process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000";
 
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
@@ -21,6 +25,21 @@ export default function FileUploader() {
     setLoading(true);
 
     try {
+      const formData = new FormData();
+      console.log("FILE: ", selectedFile);
+      formData.append("csv_file", selectedFile);
+      console.log(formData)
+      const res = await fetch(`${BACKEND_ROUTE}/upload`, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!res.ok) {
+        console.error(`Upload failed with this error: ${res.status}`);
+        return;
+      }
+      const data = await res.json();
+      console.log("upload data: ", data);
       // API logic goes here
     } catch (err) {
       console.error(err);
@@ -33,8 +52,7 @@ export default function FileUploader() {
   return (
     <div>
       <input type="file" accept=".csv" onChange={onFileChange} />
-
-      <button onClick={handleUpload}></button>
+      <button onClick={handleUpload}>Upload!!</button>
       {loading && "Loading..."}
     </div>
   );
