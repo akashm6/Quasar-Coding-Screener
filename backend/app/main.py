5e03281f3b87f6f3915cdf0c8c2ae87abab96558
Akash Mohan <akhikm4@gmail.com>
2025-09-23 21:15:24 -0500

    Correct Middleware setup for FastAPI entrypoint file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.upload import router
+import os
+from dotenv import load_dotenv
+from app.upload.upload import router
+
+load_dotenv()
+
+final_frontend_url = os.getenv("FINAL_FRONTEND_URL") or None
 
 app = FastAPI()
 
-app.add_middleware(CORSMiddleware(
+app.add_middleware(
+    CORSMiddleware,
     allow_origins=['*'],
     allow_headers=['*'],
     allow_methods=['*'],
     allow_credentials=True
-    )
 )
 
-app.include_router(router, prefix = "/api")
+app.include_router(router)
 
