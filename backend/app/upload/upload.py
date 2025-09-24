c333e8712b230d686ed28191bcbe05e50bd57866
Akash Mohan <akhikm4@gmail.com>
2025-09-23 21:15:55 -0500

    Fixed errors in CSV processing relating to NaNs and unclean data

--- backend/app/upload/upload.py
+++ backend/app/upload/upload.py
@@ -2,13 +2,17 @@
 from fastapi import File, UploadFile, APIRouter
 import pandas as pd
 from app.signals.signals import process_signals
+import numpy as np
 
 router = APIRouter()
 
 @router.post("/upload")
 async def process_csv(csv_file: UploadFile = File(...)):
     df = pd.read_csv(csv_file.file, comment='#')
-    #print("Read csv: ", df)
+    
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df = df.fillna(0)
+
     plot_traces, ecg_map = process_signals(df)
     
     return {"ecg_map": ecg_map, "traces": plot_traces}
