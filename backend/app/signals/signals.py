b33e52bf907ea65a0a5c7bb0ee1a2996f145796c
Akash Mohan <akhikm4@gmail.com>
2025-09-26 12:55:38 -0500

    Splits eeg, ecg, and cm channels and filters out all irrelevant columns for improved frontend response

--- backend/app/signals/signals.py
+++ backend/app/signals/signals.py
@@ -1,37 +1,32 @@
-# All signal processing logic goes here
 import pandas as pd
-from collections import defaultdict
+import numpy as np
+
+IGNORE_COLS = {"Trigger", "Time_Offset", "ADC_Status", "ADC_Sequence", "Event", "Comments"}
 
 def process_signals(df: pd.DataFrame):
-    
     all_cols = list(df.columns)
-    time = all_cols[0]
-    eeg_cols = all_cols[1:]
-    
-    traces = []
-    ecg_map = defaultdict()
-    
-    for c in eeg_cols:
-        if c.startswith("X1") or "LEOG" in c:
-            ecg_map["LEFT_ECG"] = c
-        elif c.startswith("X2") or "REOG" in c:
-            ecg_map["RIGHT_ECG"] = c
-            
-    x = df[time].tolist()
-    
-    for c in eeg_cols:
-        y = df[c].astype(float).tolist()
-        trace = {
-            "name": c,
-            "x": x,
-            "y": y
-        }
-        traces.append(trace)
-        
-    return traces, ecg_map
-    
+    time_col = all_cols[0] 
+    channel_cols = [c for c in all_cols[1:] if c not in IGNORE_COLS]
 
-    
-    
-    
-    
+    eeg_channels = {}
+    ecg_channels = {}
+    cm_channel = {}
+
+    for c in channel_cols:
+        if c.startswith("X1") or "LEOG" in c:
+            ecg_channels["LEFT_ECG"] = df[c].tolist()
+        elif c.startswith("X2") or "REOG" in c:
+            ecg_channels["RIGHT_ECG"] = df[c].tolist()
+        elif c.startswith("CM"):
+            cm_channel["CM"] = df[c].tolist()
+        else:
+            eeg_channels[c] = df[c].tolist()
+
+    time = df[time_col].tolist()
+
+    return {
+        "time": time,
+        "eeg_channels": eeg_channels,
+        "ecg_channels": ecg_channels,
+        "cm_channel": cm_channel,
+    }
